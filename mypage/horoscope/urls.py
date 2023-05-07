675f2d667317789ddd048a2065d563ac06a0861f
Artem <120787936+ArtemG8@users.noreply.github.com>
2023-05-07 17:58:48 +0300

    Update urls.py

--- mypage/horoscope/urls.py
+++ mypage/horoscope/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from . import views as views_horo
-from week_days import views as views_days
 
 urlpatterns = [
     path('horoscope/type/', views_horo.get_type),
-
-    # path('<int:sign_zodiac>/', views_horo.get_info_number),
     path('<str:sign_zodiac>/', views_horo.get_info_word, name='horoscope_name'),
-
-# path('<int:sign_zodiac>/', views_horo.get_info_number),
 ]
